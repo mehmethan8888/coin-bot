779e8f4579307a761b0042ceb6615d412e5a796f
mehmethan8888 <mehmethan.888@gmail.com>
2026-08-13 14:02:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,10 +7,9 @@ bot = telebot.TeleBot("8790856684:AAHjjEb0nqHPpbNMONbyH3g4GVUUu_B0m5E")
 
 if __name__ == "__main__":
     print("Bot çalışıyor")
-    
+
     @bot.message_handler(commands=['start'])
     def send_welcome(message):
         bot.reply_to(message, "Bot çalışıyor! Hazırım.")
-    
-    bot.infinity_polling()
 
+    bot.infinity_polling()
